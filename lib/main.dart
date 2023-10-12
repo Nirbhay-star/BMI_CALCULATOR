db9d3c01fc224962a1434dda1c6c365c83aa815b
Nirbhay Pandey <nirbhaypandey024@gmail.com>
2023-10-12 14:35:28 +0530

    add in main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 47, 2, 124)),
         useMaterial3: true,
       ),
       home: const MyHomePage(
@@ -41,6 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
   var ftController = TextEditingController();
   var inchController = TextEditingController();
   var result = '';
+  var bgcolor = Colors.indigo.shade200;
 
   void _incrementCounter() {
     setState(() {
@@ -52,82 +54,101 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.indigo,
         title: Center(child: Text("Your BMI")),
       ),
-      body: Center(
-        child: Container(
-          width: 300,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: Text("BMI",
-                    style:
-                        TextStyle(fontSize: 30, fontWeight: FontWeight.w700)),
-              ),
-              TextField(
-                controller: wtController,
-                decoration: InputDecoration(
-                  label: Center(child: Text('Enter Your Weight')),
-                  prefixIcon: Icon(Icons.line_weight),
+      body: Container(
+        color: bgcolor,
+        child: Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Text("BMI",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.w700)),
                 ),
-                keyboardType: TextInputType.numberWithOptions(),
-              ),
-              SizedBox(
-                height: 18,
-              ),
-              TextField(
-                controller: ftController,
-                decoration: InputDecoration(
-                  label: Center(child: Text('Enter Your Height in feets')),
-                  prefixIcon: Icon(Icons.height),
+                TextField(
+                  controller: wtController,
+                  decoration: InputDecoration(
+                    label: Center(child: Text('Enter Your Weight')),
+                    prefixIcon: Icon(Icons.line_weight),
+                  ),
+                  keyboardType: TextInputType.numberWithOptions(),
                 ),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(
-                height: 18,
-              ),
-              TextField(
-                controller: inchController,
-                decoration: InputDecoration(
-                  label: Center(child: Text('Enter Your Height in Inches')),
-                  prefixIcon: Icon(Icons.height),
+                SizedBox(
+                  height: 18,
                 ),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    var wt = wtController.text.toString();
-                    var ft = ftController.text.toString();
-                    var inch = inchController.text.toString();
-                    if (wt != "" && ft != '' && inch != '') {
-                      var iwt = int.parse(wt);
-                      var ift = int.parse(ft);
-                      var Iinch = int.parse(inch);
-
-                      var tinch = (ift * 12) + Iinch;
-                      var tcm = tinch * 2.54;
-                      var tm = tcm / 100;
-                      var bmi = iwt / (tm * tm);
-                      setState(() {
-                        result = 'Your bmi is $bmi';
-                      });
-                    } else {
-                      setState(() {
-                        result = 'Please fill all the required blank';
-                      });
-                    }
-                  },
-                  child: Text('Calculate')),
-              Text(
-                result,
-                style: TextStyle(fontSize: 15),
-              )
-            ],
+                TextField(
+                  controller: ftController,
+                  decoration: InputDecoration(
+                    label: Center(child: Text('Enter Your Height in feets')),
+                    prefixIcon: Icon(Icons.height),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                TextField(
+                  controller: inchController,
+                  decoration: InputDecoration(
+                    label: Center(child: Text('Enter Your Height in Inches')),
+                    prefixIcon: Icon(Icons.height),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Center(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        var wt = wtController.text.toString();
+                        var ft = ftController.text.toString();
+                        var inch = inchController.text.toString();
+                        if (wt != "" && ft != '' && inch != '') {
+                          var iwt = int.parse(wt);
+                          var ift = int.parse(ft);
+                          var Iinch = int.parse(inch);
+                
+                          var tinch = (ift * 12) + Iinch;
+                          var tcm = tinch * 2.54;
+                          var tm = tcm / 100;
+                          var bmi = iwt / (tm * tm);
+                          var msg = '';
+                          if (bmi > 25) {
+                            msg = 'You are over weight';
+                            bgcolor = Colors.orange.shade200;
+                          } else if (bmi < 18) {
+                            msg = 'You are under weight';
+                            bgcolor = Colors.red.shade300;
+                          } else {
+                            msg = "You're healthy";
+                            bgcolor = Colors.green.shade300;
+                          }
+                          setState(() {
+                            result = '$msg \n Your bmi is $bmi';
+                          });
+                        } else {
+                          setState(() {
+                            result = 'Please fill all the required blank';
+                          });
+                        }
+                      },
+                      child: Text('Calculate')),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    result,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
